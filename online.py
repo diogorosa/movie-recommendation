80f85e25b74cbbca2dc253a25c67a66206141b8b
diogorosa <diogo.m.c.rosa@gmail.com>
2014-05-09 17:12:30 +0100

    Add simple command line interface
    Signed-off-by: diogorosa <diogo.m.c.rosa@gmail.com>

---
+++ online.py
@@ -0,0 +1,107 @@
+import recommender as r
+import prediction_compute as p
+from pymongo import MongoClient
+import sys
+import datetime
+from time import strptime
+
+general_user = '0'
+client = MongoClient()
+db = client.recommender_db
+def show_movie_collection():
+    movies = db.movies
+    for movie in movies.find({"ratings":{'$elemMatch':{"u_id":general_user}}}, {"m_id":1, "m_title":1, "ratings":{"$elemMatch":{"u_id":general_user}}}).limit(10):
+      print "--------------movie-------------"
+      print "title --> " + movie['m_title']
+      print "id --> " + movie['m_id']
+      print "rating --> " + str(movie['ratings'][0]["score"])
+
+
+def get_user_id():
+    global general_user
+    user_id = 0
+    while user_id == 0:
+      print "\n-------------- insert user id ---------------"
+      user_id = int(raw_input('u_id: '))
+      print user_id
+    general_user = str(user_id)
+
+
+def get_m_id():
+    movies = db.movies
+    m_id = 0
+    while m_id == 0:
+      print "\n-------------- movie id to rate ---------------"
+      m_id = int(raw_input('m_id: '))
+      print "option --->" + str(m_id)
+    if movies.find_one({"m_id": str(m_id)}):
+        return m_id
+    else:
+        return
+
+def rate_movies():
+    m_id = get_m_id()
+    rate = 0
+    while rate == 0:
+      print "\n-------------- Insert Rating min:0 max:5 ---------------"
+      rate = int(raw_input('value: '))
+      print "rated --->" + str(rate)
+    movies = db.movies
+    ratings = db.ratings
+    movies.update({"m_id":str(m_id)},
+                        {'$push':{'ratings': {"u_id":general_user,
+                                                "score":rate,
+                                                "timestamp":datetime.datetime.utcnow()}}})
+    rating = {"u_id":general_user, "m_id":str(m_id), "score":rate, "timestamp":datetime.datetime.utcnow()}
+    ratings.insert(rating)
+
+def unseen_movies():
+    movies = db.movies
+    unseen = movies.find({"ratings.u_id":{"$ne":general_user}})
+    for movie in unseen.limit(20):
+      print "--------------movie-------------"
+      print "title --> " + movie['m_title']
+      print "id --> " + movie['m_id']
+    print "unseen movies until now -----> " + str(unseen.count())
+
+def see_movie():
+    m_id = get_m_id()
+    movies = db.movies
+    movie = movies.find_one({"ratings.u_id":general_user, "m_id":str(m_id)}, {"m_id":1, "m_title":1, "ratings":{"$elemMatch":{"u_id":general_user}}})
+    if movie:
+        print "--------------movie-------------"
+        print "title --> " + movie['m_title']
+        print "id --> " + movie['m_id']
+        print "rating --> " + str(movie['ratings'][0]["score"])
+    else:
+        movie = movies.find_one({"m_id":str(m_id)})
+        print "--------------movie-------------"
+        print "title --> " + movie['m_title']
+        print "id --> " + movie['m_id']
+        print "Rate this movie !!!!"
+    select = 0
+    s_options = ['','y','n']
+    while select == 0:
+      print "\n-------------- rate? ---------------"
+      for i, option in enumerate(s_options):
+          print('%s. %s' % (i, option))
+      select = int(raw_input('? '))
+    if select == 1:
+        return
+    else:
+        rate_movies()
+
+def exit():
+    sys.exit(0)
+
+options = ['insert u_id', 'seen movies', 'unseen movies', 'rate movies', 'see movie','View Suggestions','Quit']
+callbacks = [get_user_id, show_movie_collection, unseen_movies, rate_movies, see_movie, exit]
+
+while True:
+    print "\n-------------- Movie Recommender ---------------"
+    for i, option in enumerate(options):
+        print('%s. %s' % (i, option))
+    choice = int(raw_input('your choice? '))
+    print choice
+    callbacks[choice]()
+
